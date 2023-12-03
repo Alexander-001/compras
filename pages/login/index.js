d924f09abd03a7da5281b7537efb0d5e7080ea28
Alexander Ortiz <CL198785156@enel.com>
2023-12-02 22:31:30 -0300

    First commit

---
+++ pages/login/index.js
@@ -0,0 +1,24 @@
+const btnIniciar = document.querySelector("#btnIniciar");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+btnIniciar.addEventListener("click", () => {
+  const valueEmail = email.value;
+  const { isError, errorMessage } = isValidEmail(valueEmail);
+  if (isError) {
+    alert(errorMessage);
+  }
+
+  window.location.assign("/home");
+});
+
+const isValidEmail = (email) => {
+  const returnData = { errorMessage: "", isError: false };
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!regex.test(email)) {
+    returnData.errorMessage = "El correo no es valido";
+    returnData.isError = true;
+  }
+
+  return returnData;
+};
